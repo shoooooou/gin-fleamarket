1c7f0a1a1a455f534d91722c5e22684afbd66622
s.nakamura <shotenba@gmail.com>
2024-07-14 15:03:07 +0900

    モデルを作成

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-fleamarket/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		items := []models.Item{
+			{ID: 1, Name: "商品1", Price: 1000, Description: "商品１", SoldOut: false},
+			{ID: 2, Name: "商品2", Price: 2000, Description: "商品2", SoldOut: true},
+			{ID: 3, Name: "商品3", Price: 3000, Description: "商品3", SoldOut: false},
+		}
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
