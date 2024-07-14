f0911503d3f877c40a9b55e98d9793124b7c56fd
s.nakamura <shotenba@gmail.com>
2024-07-14 16:38:01 +0900

    商品検索APIを作成

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"gin-fleamarket/controllers"
 	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	items := []models.Item{
+		{ID: 1, Name: "商品1", Price: 1000, Description: "商品１", SoldOut: false},
+		{ID: 2, Name: "商品2", Price: 2000, Description: "商品2", SoldOut: true},
+		{ID: 3, Name: "商品3", Price: 3000, Description: "商品3", SoldOut: false},
+	}
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemsService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemsService)
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		items := []models.Item{
-			{ID: 1, Name: "商品1", Price: 1000, Description: "商品１", SoldOut: false},
-			{ID: 2, Name: "商品2", Price: 2000, Description: "商品2", SoldOut: true},
-			{ID: 3, Name: "商品3", Price: 3000, Description: "商品3", SoldOut: false},
-		}
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/items", itemController.FindAll)
+	r.GET("/items/:id", itemController.FindById)
 	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080
 }
